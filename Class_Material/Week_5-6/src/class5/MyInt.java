0db9a879504f500d131606e9c66ea343056b0fcb
boaz ben-moshe <benmo@g.ariel.ac.il>
2021-10-10 20:11:22 +0300

    Class5-6 - basic Threads - several Examples (very few JUnits)

---
+++ Class_Material/Week_5-6/src/class5/MyInt.java
@@ -0,0 +1,13 @@
+package class5;
+public class MyInt {
+    private int _t;
+    public MyInt(int t) {_t = t;}
+    synchronized public int get() {return _t;}
+    synchronized public void inc() {
+        _t++;
+        if(_t%1000==0) {
+            long id = Thread.currentThread().getId();
+         //   System.out.println("id: "+id+"  "+_t);
+        }
+    }
+}
