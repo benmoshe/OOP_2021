935b6700b80bf493407f7395821acd248cb9a21f
achiyazigi <matanelcohen0@walla.com>
2021-11-16 04:50:55 +0200

    bonus code for week 6

---
+++ Class_Material/Week_6/T6-AchiyaZigi/threads/basic/CarRunnable.java
@@ -0,0 +1,32 @@
+package threads.basic;
+import java.lang.Thread;
+public class CarRunnable implements Runnable{
+    private int x = 0;
+    private int speed = 0;
+    public CarRunnable(int speed){
+        this.speed = speed;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName());
+        for (int i = 0; i < Main.screenSize; i++) {
+            this.x += this.speed;
+            
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        
+    }
+
+}
---
+++ Class_Material/Week_6/T6-AchiyaZigi/threads/basic/Main.java
@@ -0,0 +1,46 @@
+package threads.basic;
+
+public class Main {
+    public static final int screenSize = 60;
+    static final String rightCarString = "[0=0>";
+    static final String leftCarString = "(0^0]";
+
+    public static void main(String args[]) {
+        System.out.println("hello world");
+        CarRunnable carToRight = new CarRunnable(2);
+        CarRunnable carToLeft = new CarRunnable(1);
+        Thread tLeft = new Thread(carToRight);
+        Thread tRight = new Thread(carToLeft);
+        tLeft.start();
+        tRight.start();
+
+        for (int i = 0; i < screenSize; i++) {
+
+            int dist = screenSize - carToRight.getX() - carToLeft.getX();
+            String distBetwin = " ".repeat(Math.abs(dist));
+
+            if (dist >= 0) {
+                String distRCar = " ".repeat(carToRight.getX());
+                System.out.println(distRCar + rightCarString + distBetwin + leftCarString);
+
+            }
+            else {
+                String distLCar = " ".repeat(screenSize - carToLeft.getX());
+                System.out.println(distLCar + leftCarString + distBetwin + rightCarString);
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("\033[H\033[2J");
+        }
+        try {
+            tLeft.join();
+            tRight.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(carToRight.getX());
+    }
+}
