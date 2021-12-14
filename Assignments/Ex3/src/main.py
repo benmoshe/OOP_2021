8a2de4d6716bd91128e786a06345fa4253f702a3
boaz ben-moshe <benmo@g.ariel.ac.il>
2021-12-14 20:02:13 +0200

    Ex3 - main example (updated with the file names).

--- Assignments/Ex3/src/main.py
+++ Assignments/Ex3/src/main.py
@@ -67,7 +67,7 @@ def check2():
       :return:
       """
     g_algo = GraphAlgo()
-    file = '../data/A5'
+    file = '../data/A5.json'
     g_algo.load_from_json(file)
     g_algo.get_graph().remove_edge(13, 14)
     g_algo.save_to_json(file + "_edited")
